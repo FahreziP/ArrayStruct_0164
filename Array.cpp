2b363873726351c0afb8f1badd421e6277e8b76d
FahreziP <fahrezipurwadianitra@gmail.com>
2025-04-14 16:03:10 +0700

    membuat nilai akhir menjadi looping dan tampilan akhirnya

--- Array.cpp
+++ Array.cpp
@@ -12,4 +12,22 @@ int main(){
     matkul[0] = "Logika teknik pemrograman";
     cout << "Nama matakuliah : " << matkul[0] << endl;
     
+    for(int i = 0; i < 2; i++){
+        cout << "Nilai akhir ke:" << i+1 << "=" << nilaiAkhir[i] << endl;
+    }
+
+    cout << "Input Grade" << endl;
+    for(int i = 0; i < 5; i++){
+        cout << "Masukkan Grade ";
+        cin >> grade[i];
+    }
+
+    cout << endl;
+    cout << "Tampilkan Grade" << endl;
+    cout << endl;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cout << "Grade = " << grade[i] << endl;
+    }
 }
